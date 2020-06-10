be99c9dc7d04e2617874cf8917ee60068d6ae92e
Danielle <ddempsey@perennia.ca>
2020-06-09 21:09:56 -0300

    Can now read in .csv or .xlsx (must be specified in args) We are going to start exporting HOBO data in csv format because it is in true UTC.
    Removed tz.UTC from argumnets because it only applied to the xlsx files. Now if file.type == "xlsx", tz.UTC is automatically set to TRUE, and is FALSE otherwise. As we convert our workflow to csv files, the convert_to_real_UTC() function will be removed
    
    Some files have Plot Title as the first line. If so, function will remove (to be more flexible in case some files have it and others dont)
    
    Added in a format to parse_date_time so it can read in dates exported directly from HOBOware default. May need to add others

--- R/compile_HOBO_data.R
+++ R/compile_HOBO_data.R
@@ -2,10 +2,12 @@
 #'@description This function compiles the data from a HOBO deployment at
 #'  different depths into a single dataframe or spreadsheet.
 #'@details
-#'  HOBO data should be exported in GMT+00. Note that the HOBO
-#'  timestamp accounts for daylight savings time, but true UTC does not. This
-#'  function gives the option to convert to true UTC time with the
-#'  \code{real_UTC} argument.
+#'       # add wanring if dup datetimes
+#'       could make date format an argument
+#'HOBO data should be exported in GMT+00. Note that the HOBO timestamp
+#'  accounts for daylight savings time, but true UTC does not. This function
+#'  gives the option to convert to true UTC time with the \code{real_UTC}
+#'  argument.
 #'
 #'  The functions used to convert to true UTC are
 #'  \code{convert_HOBO_datetime_to_real_UTC()} and \code{dates_to_fix()}, which
@@ -30,7 +32,8 @@
 #'
 #'@param path.HOBO File path to the Hobo folder. All of the excel files in the
 #'  Hobo folder should be data extracted from the HOBO software. The name of
-#'  each excel file must be the serial number of the sensor.
+#'  each excel file must be the serial number of the sensor, and the excel files
+#'  must all have the same extension (either .csv or .xlsx)
 #'@param area.name Area where the HOBO was deployed.
 #'@param serial.table A table with the serial number of each HOBO (first column)
 #'  and corresponding depth at which it was deployed (second column).
@@ -39,12 +42,14 @@
 #'@param trim Logical value indicating whether to trim the data to the dates
 #'  specified in \code{deployment.range}. (Note: four hours are added to the
 #'  retrieval date to account for AST, e.g., in case the sensor was retrieved
-#'  after 20:00 AST, which is 00:00 UTC the next day.) Default is \code{trim = TRUE}.
-#'@param tz.UTC Logical value indicating whether to convert timestamp to UTC.
-#'  Note that the HOBO timestamp GMT+00 accounts for daylight savings time, but
-#'  true UTC does not. \code{true_UTC = FALSE} will return the timestamps as
-#'  exported from the HOBO software. \code{true_UTC = TRUE} converts the
-#'  timestamp to true UTC time. Default is \code{real_UTC = TRuE}.
+#'  after 20:00 AST, which is 00:00 UTC the next day.) Default is \code{trim =
+#'  TRUE}.
+#'@param file.type Character string indicating whether the HOBO data is in .csv
+#'  or .xlsx format. All data files that are being compiled must have the same
+#'  file extension. Note that the HOBO software exports csv files in true UTC,
+#'  but xlsx files account for daylight savings time. This function will
+#'  automatically convert the datetimes in xlsx files to true UTC. Default is
+#'  \code{file.type = "csv"}. Alternative is \code{file.type = "xlsx"}.
 #'@param export.csv Logical value indicating whether to export the compiled data
 #'  as a .csv file. If \code{export.csv = TRUE}, the compiled data will not be
 #'  returned to the global environment. Default is \code{export.csv = FALSE}.
@@ -70,7 +75,7 @@ compile_HOBO_data <- function(path.HOBO,
                               serial.table,
                               deployment.range,
                               trim = TRUE,
-                              tz.UTC = TRUE,
+                              file.type = "csv",
                               export.csv = FALSE){
 
   names(serial.table) <- c("SERIAL", "VAR_DEPTH")
@@ -86,11 +91,23 @@ compile_HOBO_data <- function(path.HOBO,
   # initialize dateframe for storing the output
   HOBO_dat <- data.frame(INDEX = as.character())
 
+
+# List files to be compiled -----------------------------------------------
+
   # finish path
   path.HOBO <- file.path(paste(path.HOBO, "/Hobo", sep = ""))
 
   # list files .xlsx files in the data folder
-  dat.files <- list.files(path.HOBO, all.files = FALSE, pattern = "*.xlsx")
+  if(file.type == "xlsx"){
+    dat.files <- list.files(path.HOBO, all.files = FALSE, pattern = "*.xlsx")
+    tz.UTC <- TRUE
+  }
+
+  # list files .csv files in the data folder
+  if(file.type == "csv"){
+    dat.files <- list.files(path.HOBO, all.files = FALSE, pattern = "*.csv")
+    tz.UTC <- FALSE
+  }
 
   # remove files that start with "~"
   if(any(substring(dat.files, 1, 1)== "~")) {
@@ -104,12 +121,35 @@ compile_HOBO_data <- function(path.HOBO,
   # loop over each HOBO file
   for(i in 1:length(dat.files)) {
 
-    # import HOBO file i
-    hobo.i_dat <- read_excel(paste(path.HOBO, dat.files[i], sep = "/"), col_names = FALSE)
 
-    # extract serial number
-    serial.i <- hobo.i_dat[1,1] %>%
-      tidyr::separate(col = 1, into = c(NA, "SERIAL"), sep = ": ", remove = TRUE)
+# Import Data -------------------------------------------------------------
+
+    file.name <- dat.files[i]
+
+    # import HOBO file i. Remove the first row if the first cell is the Plot Title
+    if(file.type == "xlsx") {
+      hobo.i_dat <- read_excel(paste(path.HOBO, file.name, sep = "/"), col_names = FALSE)
+
+      if(hobo.i[1,1] != "#")  hobo.i <- hobo.i_dat %>% slice(-1)
+    }
+    if(file.type == "csv") {
+      hobo.i_dat <- read_csv(paste(path.HOBO,  file.name, sep = "/"), col_names = FALSE, skip = 1)
+
+      if(hobo.i_dat[1,1] != "#") {
+        hobo.i_dat <- read_csv(paste(path.HOBO,  file.name, sep = "/"), col_names = FALSE)
+      }
+    }
+
+    # select the first three columns
+    hobo.i <- hobo.i_dat %>%
+      select(c(1:3))
+
+
+# Extract metadata --------------------------------------------------------
+
+    # extract serial number from file name
+    serial.i <- data.frame(file.name) %>%
+      tidyr::separate(col = file.name, into = c("SERIAL", NA), sep = "\\.", remove = TRUE)
     serial.i <- paste("HOBO-", serial.i$SERIAL, sep = "")
 
     # use serial number to identify the variable and depth (from serial.table)
@@ -118,25 +158,44 @@ compile_HOBO_data <- function(path.HOBO,
       select(VAR_DEPTH)
     variable_depth <- variable_depth$VAR_DEPTH
 
-    # remove plot title row and select the first three columns
-    hobo.i <- hobo.i_dat %>%
-      slice(-1) %>%
-      select(c(1:3))
-
     # extract date column header (includes GMT offset)
-    date_ref <- hobo.i[1,2]$...2
+    #date_ref <- hobo.i[1,2]$...2
+    date_ref <- hobo.i[1,2]$X2
     # extract temperature column header (includes units)
-    temp_ref <- data.frame(hobo.i[1,3]$...3) %>%
+    temp_ref <- data.frame(hobo.i[1,3]) %>%
       rename("temp_ref" = 1) %>%
       separate(col = "temp_ref", into = c("temp_ref", NA), sep = 8)
     temp_ref <- temp_ref$temp_ref
 
-    # format data
+
+# Format data -------------------------------------------------------------
+
     hobo.i <- hobo.i %>%
-      slice(-1) %>%                                                     # remove column headings
-      select(INDEX = 1, DATE = 2, TEMPERATURE = 3) %>%                  # rename columns (will be dropped for export)
-      mutate(DATE = convert_to_datetime(DATE),                          # convert DATE to datetime
-             TEMPERATURE = round(as.numeric(TEMPERATURE), digits = 3))  # round temperature data to 3 decimal places
+      slice(-1) %>%                                       # remove column headings
+      select(INDEX = 1, DATE = 2, TEMPERATURE = 3)        # rename columns (will be dropped for export)
+
+    # if the date can be converted to class numeric, then it is stored as a number in Excel
+    ## and we have to use janitor::convert_to_datetime to convert to POSIXct.
+    # Otherwise the date should be a character string that can be converted to POSIXct using
+    ## lubridate::parse_date_time()
+
+    date_format <- hobo.i$DATE[1]
+    if(!is.na(suppressWarnings(as.numeric(date_format)))) {
+
+      hobo.i <- hobo.i %>%
+        mutate(DATE = convert_to_datetime(as.numeric(DATE)))
+
+    } else{
+
+      hobo.i <- hobo.i %>%
+        mutate(DATE = parse_date_time(DATE,
+                                      orders = c("ymd IMS p", "Ymd HM", "Ymd HMS")))
+    }
+
+    #hobo.i <- hobo.i %>%
+     # mutate(DATE = format(DATE,  "%Y-%m-%d %H:%M:%S"),                # format date to "2020-06-09 20:16:30"
+      #       TEMPERATURE = round(as.numeric(TEMPERATURE), digits = 3)) # round temperature data to 3 decimal places
+
 
     # un-account for daylight savings time
     # (subtract 1 hour from each datetime within the range of DST)
@@ -147,21 +206,26 @@ compile_HOBO_data <- function(path.HOBO,
     # (e.g., in case the sensor was retrieved after 20:00 AST, which is 00:00 UTC **The next day**)
     if(trim == TRUE) {
       hobo.i <- hobo.i %>%
-        filter(DATE >= start.date, DATE <= (end.date + hours(4)))
+        filter(DATE >= start.date, DATE <= (end.date + hours(4))) %>%
+        mutate(INDEX = c(1:n()))
     }
 
-    # convert columns to class character so can add in the column headings
+    # convert columns to class character so can add in the meta data
     hobo.i <- hobo.i %>%
-      mutate(DATE = as.character(DATE),
-             INDEX = as.character(round(as.numeric(INDEX), digits = 0)), # make sure INDEX will have the same class and format for each sheet
-             PLACEHOLDER = as.character(TEMPERATURE))  %>%
-      select(-TEMPERATURE) %>%
+      mutate(INDEX = as.character(round(as.numeric(INDEX), digits = 0)), # make sure INDEX will have the same class and format for each sheet
+             DATE = format(DATE,  "%Y-%m-%d %H:%M:%S"),
+             PLACEHOLDER = as.character(round(as.numeric(TEMPERATURE), digits = 3)))  %>%
+      select(INDEX, DATE, PLACEHOLDER) %>%
       add_metadata(row1 = deployment.range, row2 = serial.i, row3 = variable_depth, row4 = c(date_ref, temp_ref))
 
     # merge data on the INDEX row
     HOBO_dat <- full_join(HOBO_dat, hobo.i, by = "INDEX")
 
   } # end for loop
+
+
+# Return compiled data ----------------------------------------------------
+
 
   if(export.csv == TRUE){
     # format start date for file name
