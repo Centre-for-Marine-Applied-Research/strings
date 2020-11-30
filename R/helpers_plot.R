92c8bb6c6b31067b1e0797a1ca3c900546a78729
Danielle Dempsey <ddempsey@perennia.ca>
2020-11-30 15:21:46 -0400

    move some lines to here from body of County Report.Rmd to improve clarity of that file

--- R/helpers_plot.R
+++ R/helpers_plot.R
@@ -1,21 +1,28 @@
 #'@title Returns nice major and minor breaks and label format based on timespan
 #'  of the data
-#'@details If \code{timespan} is less than or equal to 60 days, the major and
-#'  minor breaks will be every 2 weeks; if time span is greater than 60 days but
-#'  less than or equal to 120 days, the major and minor breaks will be every
-#'  month; if time span is greater than 120 days, the major breaks will be 2
-#'  months and the minor breaks will be 1 month.
-#'@param timespan Number of days of data to be plotted.
+#'@details If the timespan in the \code{TIMESTAMP} column of \code{dat.tidy} is
+#'  less than or equal to 60 days, the major and minor breaks will be every 2
+#'  weeks; if time span is greater than 60 days but less than or equal to 120
+#'  days, the major and minor breaks will be every month; if time span is
+#'  greater than 120 days, the major breaks will be 2 months and the minor
+#'  breaks will be 1 month.
+#'@param dat.tidy Data to be plotted, in tidy format, as returned by the
+#'  function \code{convert_to_tidydata()}. Must include the column
+#'  \code{TIMESTAMP} (POSIXct).
 #'@return Returns a dataframe with 1 observation of 3 variables
 #'  \code{date.breaks.major}, \code{date.breaks.minor},
 #'  \code{date.labels.format}.
 
-#'@family OpenData
+#'@family plot
 #'@author Danielle Dempsey
 #'@export
 
 
-get_xaxis_breaks <- function(timespan){
+get_xaxis_breaks <- function(dat.tidy){
+
+  # timespan of the data
+  timespan <- difftime(max(dat.tidy$TIMESTAMP), min(dat.tidy$TIMESTAMP), units = "days")
+  timespan <- unclass(timespan)[1]
 
   if(timespan <= 60){
     date.breaks.major = "2 week"
@@ -29,8 +36,14 @@ get_xaxis_breaks <- function(timespan){
     date.labels.format = "%y-%b"
   }
 
-  if(timespan > 120){
+  if(timespan > 120 & timespan <= 240){
     date.breaks.major = "2 month"
+    date.breaks.minor = "1 month"
+    date.labels.format = "%y-%b"
+  }
+
+  if(timespan > 240){
+    date.breaks.major = "4 month"
     date.breaks.minor = "1 month"
     date.labels.format = "%y-%b"
   }
