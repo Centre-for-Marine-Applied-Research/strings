0c439e199d8d0a254fa5573d38eb0b97da156bf7
Danielle Dempsey <ddempsey@perennia.ca>
2021-09-16 08:13:07 -0300

    fix: generalized how the function recognizes the sensor names to account for difference in capitalization and spacing

--- R/read_deployment_log.R
+++ R/read_deployment_log.R
@@ -44,25 +44,26 @@
 #'  If there is more than one eligible file (csv, .xlsx or .xls) in the Log
 #'  folder, the function will stop with an error.
 #'
-#'@param path.log File path to the Log folder.
-#'@return Returns a list with 5 elements. \code{deployment.dates} is a dataframe
+#' @param path.log File path to the Log folder.
+#' @return Returns a list with 5 elements. \code{deployment.dates} is a dataframe
 #'  with two columns: \code{start.date} (the date of deployment) and
 #'  \code{end.date} (date of retrieval). \code{area.info} is a dataframe with
 #'  five columns: \code{waterbody}, \code{latitude}, \code{longitude},
 #'  \code{station}, and \code{lease}. \code{HOBO}, \code{aM}, and \code{vemco}
 #'  are each a dataframe with two columns: \code{SENSOR} (serial number) and the
 #'  corresponding \code{DEPTH} (depth of deployment in m).
-#'@family compile
-#'@author Danielle Dempsey
+#' @family compile
+#' @author Danielle Dempsey
 #'
-#'@importFrom tidyr separate
-#'@importFrom readxl read_excel
-#'@importFrom readr read_csv
-#'@importFrom lubridate ymd
-#'@import dplyr
-#'@export
+#' @importFrom tidyr separate
+#' @importFrom stringr str_replace str_detect
+#' @importFrom readxl read_excel
+#' @importFrom readr read_csv
+#' @importFrom lubridate ymd
+#' @import dplyr
+#' @export
 
-#'@examples
+#' @examples
 #' # path to "Log" folder
 #' path <- system.file("extdata", package = "strings")
 #'
@@ -142,20 +143,47 @@ read_deployment_log <- function(path.log){
                           station  = log$Location_Description[1],
                           lease = log$`Lease#`[1])
 
-  # HOBO & TidBit sensors
-  hobo.sensors <- c("HOBO Pro V2", "HOBO pro V2", "HOBO_Pro_V2",
-                    "HOBO DO",
-                    "TidbiT MX2303", "TidbiT MX2203")
 
-  hobos <- log %>%
-    filter(Logger_Model %in% hobo.sensors) %>%
+  sensors <- log %>%
+    mutate(Logger_Model = str_replace(Logger_Model, "_", " "),
+           Logger_Lower = tolower(Logger_Model),
+           detect_hobo = str_detect(Logger_Lower, "hobo"),
+           detect_tidbit = str_detect(Logger_Lower, "tidbit"),
+           detect_am = str_detect(Logger_Lower, "aquameasure"),
+           detect_vemco = str_detect(Logger_Lower, "vr2ar"))
+
+  # print a warning if there are any sensors in the log that are NOT recognized by this function
+  n_sensors <- sensors %>%
+    select(contains("detect")) %>%
+    apply(1, sum)
+
+  if(any(n_sensors == 0)) {
+
+    extra.sensor <- sensors[which(n_sensors == 0), "Logger_Model"]
+
+    warning(extra.sensor, " was found in the Logger_Model column of the log.
+            \nThis sensor is not recognized by the read_deployment_log() function")
+  }
+
+  # HOBO & TidBit sensors
+  # hobo.sensors <- c("HOBO Pro V2", "HOBO pro V2", "HOBO_Pro_V2", "HOBO_PRO_V2",
+  #                   "HOBO DO",
+  #                   "TidbiT MX2303", "TidbiT MX2203")
+
+  hobos <- sensors %>%
+    # mutate(Logger_Model = str_replace(Logger_Model, "_", " "),
+    #        Logger_Lower = tolower(Logger_Model),
+    #        detect_hobo = str_detect(Logger_Lower, "hobo"),
+    #        detect_tidbit = str_detect(Logger_Lower, "tidbit")) %>%
+    filter(detect_hobo | detect_tidbit) %>%
+    # filter(Logger_Model %in% hobo.sensors) %>%
     select(Logger_Model, `Serial#`, Sensor_Depth) %>%
     separate(Logger_Model, into = c("sensor", NA, NA), sep = " ", fill = "right") %>%
     mutate(SENSOR = paste(sensor, `Serial#`, sep = "-"),
            numeric_depth = if_else(is.numeric(Sensor_Depth), TRUE, FALSE)) %>%
     # if depth is numeric, add "m" for meters
     mutate(DEPTH = if_else(numeric_depth == TRUE,
-                                 paste(Sensor_Depth, "m", sep = ""), as.character(Sensor_Depth))) %>%
+                           paste(Sensor_Depth, "m", sep = ""), as.character(Sensor_Depth))) %>%
     select(SENSOR, DEPTH)
 
   if(nrow(hobos) == 0){
@@ -164,12 +192,18 @@ read_deployment_log <- function(path.log){
   }
 
   # aquaMeasure sensors
-  aM.sensors <- c("aquaMeasure DOT", "aquaMeasure SAL", "aquaMeasure SST",
-                  "aquameasure DOT", "aquameasure SAL", "aquameasure SST")
+  # aM.sensors <- c("aquaMeasure DOT", "aquaMeasure_DOT", "aquaMeasure SAL",
+  #                 "aquaMeasure SST",
+  #                 "aquameasure DOT", "aquameasure SAL", "aquameasure SST")
 
-  aquaMeasures <- log %>%
-    filter(Logger_Model %in% aM.sensors) %>%
+  aquaMeasures <- sensors %>%
+    # mutate(Logger_Model = str_replace(Logger_Model, "_", " "),
+    #        Logger_Lower = tolower(Logger_Model),
+    #        detect = str_detect(Logger_Lower, "aquameasure")) %>%
+    filter(detect_am) %>%
+    #filter(Logger_Model %in% aM.sensors) %>%
     select(Logger_Model, `Serial#`, Sensor_Depth) %>%
+    mutate(Logger_Model = str_replace(Logger_Model, "_", " ")) %>%
     separate(Logger_Model, into = c("sensor", NA), sep = " ") %>%
     mutate(SENSOR = paste(sensor, `Serial#`, sep = "-"),
            numeric_depth = if_else(is.numeric(Sensor_Depth), TRUE, FALSE)) %>%
@@ -184,10 +218,10 @@ read_deployment_log <- function(path.log){
   }
 
   # vemco sensor
-  vemco.sensors <- "VR2AR"
+  #vemco.sensors <- "VR2AR"
 
-  vemcos <- log %>%
-    filter(Logger_Model %in% vemco.sensors) %>%
+  vemcos <- sensors %>%
+    filter(detect_vemco) %>%
     select(Logger_Model, `Serial#`, Sensor_Depth) %>%
     mutate(SENSOR = paste(Logger_Model, `Serial#`, sep = "-"),
            numeric_depth = if_else(is.numeric(Sensor_Depth), TRUE, FALSE)) %>%
@@ -201,17 +235,6 @@ read_deployment_log <- function(path.log){
     message("No Vemco sensors found in log")
   }
 
-  # print a warning if there are any sensors in the log that are NOT recognized by this function
-  log.sensors <- unique(log$Logger_Model)
-  script.sensors <- c(hobo.sensors, aM.sensors, vemco.sensors)
-
-  if(any(!(log.sensors %in% script.sensors))) {
-
-    extra.sensor <- log.sensors[which(!(log.sensors %in% script.sensors))]
-
-    warning(extra.sensor, " was found in the Logger_Model column of the log.
-            This sensor is not recognized by the read_deployment_log() function")
-  }
 
   # return list of deployment info
   list(deployment.dates = deployment.dates,
