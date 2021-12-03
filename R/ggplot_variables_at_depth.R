8df38a25057db9bcebd339087e3cf51ef0d06acd
Danielle Dempsey <ddempsey@perennia.ca>
2021-12-03 15:37:22 -0400

    feat: add optional colour palette
    move strip text to be axis label

--- R/ggplot_variables_at_depth.R
+++ R/ggplot_variables_at_depth.R
@@ -2,6 +2,7 @@
 #' Plot variables at depth with faceted ggplot
 #'
 #' @param dat.tidy Tidy data
+#' @param color.pal Optional color palette.
 
 #' @return ggplot object
 #' @import ggplot2
@@ -11,17 +12,18 @@
 #'
 
 
-ggplot_variables_at_depth <- function(dat.tidy){
+ggplot_variables_at_depth <- function(dat.tidy, color.pal = NULL){
 
   dat.tidy <- dat.tidy %>%
     convert_depth_to_ordered_factor()
 
-  color.pal <- get_colour_palette(dat.tidy)
+  if(is.null(color.pal)) color.pal <- get_colour_palette(dat.tidy)
   axis.breaks <- get_xaxis_breaks(dat.tidy)
 
   p <- ggplot(dat.tidy, aes(x = TIMESTAMP, y = VALUE, col = DEPTH)) +
     geom_point(size = 0.25) +
     scale_x_datetime(
+      name = "Date",
       breaks = axis.breaks$date.breaks.major,
       minor_breaks = axis.breaks$date.breaks.minor,
       date_labels =  axis.breaks$date.labels.format
@@ -36,7 +38,14 @@ ggplot_variables_at_depth <- function(dat.tidy){
 
     if(length(unique(dat.tidy$VARIABLE)) > 1) {
 
-      p <- p + facet_wrap(~VARIABLE, ncol = 1, scales = "free_y")
+      p <- p + facet_wrap(~VARIABLE, ncol = 1, scales = "free_y",
+                          strip.position = "left") +
+        theme(
+          strip.placement = "outside",
+              strip.background = element_blank(),
+              strip.text = element_text(colour = "black", size = 10)
+          ) +
+        scale_y_continuous(name = "")
     }
   }
 
