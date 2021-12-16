9d22d769bf4d75b10929041a7bc64eb5b69dc776
Danielle Dempsey <ddempsey@perennia.ca>
2021-12-16 11:54:21 -0400

    make path argument name consistent with other path args
    Remove apostrophe for Owls Head

--- R/read_opendata_csv.R
+++ R/read_opendata_csv.R
@@ -34,16 +34,22 @@ read_opendata_csv <- function(path){
 
 #' Read mooring table
 #'
-#' @param path Path to mooring table (including file name and extension).
+#' @param path.mooring Path to mooring type table (including file name and
+#'   extension).
 #'
 #' @return Mooring table with standardized WATERBODY and STATION columns.
+#'
+#' @importFrom data.table fread
+#' @importFrom dplyr distinct case_when if_else mutate select
+#' @importFrom lubridate dmy
+#'
 #' @export
 
-read_mooring_csv <- function(path){
+read_mooring_csv <- function(path.mooring){
 
-  #path <- file.path("C:/Users/Danielle Dempsey/Desktop/MOORING_table.csv")
+  #path <- file.path("Y:/Coastal Monitoring Program/Open Data/Deployment Data/MOORING_table.csv")
 
-  dat_raw <- fread(path, data.table = FALSE)
+  dat_raw <- fread(path.mooring, data.table = FALSE)
 
   dat <- dat_raw %>%
     # fix spelling discrepancies
@@ -54,7 +60,7 @@ read_mooring_csv <- function(path){
         Waterbody == "St Margarets Bay" ~ "St. Margarets Bay",
         Waterbody == "St Anns Bay" ~ "St. Ann's Bay",
         Waterbody == "St Peters Inlet" ~ "St Peter's Inlet",
-        Waterbody == "Owls Head Bay" ~ "Owl's Head Bay",
+       # Waterbody == "Owls Head Bay" ~ "Owl's Head Bay",
         TRUE ~ Waterbody
       ),
       STATION = case_when(
@@ -65,7 +71,7 @@ read_mooring_csv <- function(path){
         Station_Name == "Long beach" ~ "Long Beach",
         Station_Name == "Long Island two" ~ "Long Island 2",
         Station_Name == "Monks head" ~ "Monks Head",
-        Station_Name == "Owls head" ~ "Owl's Head",
+        Station_Name == "Owls head" | Station_Name == "Owl's Head"  ~ "Owls Head",
         Station_Name == "Pipers lake" ~ "Piper Lake",
         Station_Name ==  "Shut-in Island" ~ "Shut-In Island",
         Station_Name ==  "South side" | Station_Name == "South Side" ~ "Southside",
@@ -77,6 +83,7 @@ read_mooring_csv <- function(path){
     # CMAR corrected some Waterbodies and re-named some stations
     mutate(
       WATERBODY = case_when(
+        WATERBODY == "Tor Bay" & STATION == "Bald Rock" ~ "Whitehead Harbour",
         WATERBODY == "Whycocomagh Bay" & STATION == "Deep Basin" ~ "Whycocomagh Basin",
         WATERBODY == "Antigonish Harbour" & STATION == "Captains Pond" ~ "Grahams Cove",
         WATERBODY == "Antigonish Harbour" & STATION == "Monks Head" ~ "Monks Head Harbour",
@@ -84,6 +91,7 @@ read_mooring_csv <- function(path){
         TRUE ~ WATERBODY
       ),
       STATION = case_when(
+        WATERBODY == "Salt Bay" & STATION == "Salt Bay" ~ "Big Sluice",
         WATERBODY == "Hourglass Lake" & STATION == "Deep Point" ~ "Hourglass Lake",
         WATERBODY == "Arichat Harbour" & STATION == "667" ~ "0667",
         WATERBODY == "Strait of Canso" & STATION == "Pulp Mill 2" ~ "Pulp Mill Site 2",
@@ -91,13 +99,46 @@ read_mooring_csv <- function(path){
         WATERBODY == "Chedabucto Bay" & STATION == "Sandy Cove" ~ "Sandy Cove Chedabucto",
         TRUE ~ STATION
       )
+
     ) %>%
-    mutate(MOORING = if_else(Surface.Float. == "Y", "float", "fixed")) %>%
-    select(-Surface.Float.)
+    mutate(
+      MOORING = if_else(Surface.Float. == "Y", "float", "fixed"),
+      Depl_Date = dmy(Depl_Date),
+      Recv_Date = dmy(Recv_Date),
+
+      # fix deployment/recovery dates
+      Depl_Date = case_when(
+        WATERBODY == "Chedabucto Bay" & STATION == "Rook Island" &
+          Depl_Date == as_date("2020-05-20") ~ as_date("2020-05-21"),
+
+        WATERBODY == "St. Mary's Bay" & STATION == "Church Point 1" &
+          Depl_Date == as_date("2020-02-24") ~ as_date("2020-02-23"),
+
+        WATERBODY == "St. Mary's Bay" & STATION == "Church Point 2" &
+          Depl_Date == as_date("2020-02-24") ~ as_date("2020-02-23"),
+
+        TRUE ~ Depl_Date
+      ),
+
+      Recv_Date = case_when(
+        WATERBODY == "Chedabucto Bay" & STATION == "Rook Island" &
+          Recv_Date == as_date("2020-05-20") ~ as_date("2020-05-21"),
+        TRUE ~ Recv_Date
+      )
+    ) %>%
+    select(WATERBODY, STATION, Depl_Date, Recv_Date, MOORING) %>%
+    # remove 7 duplicate rows
+    distinct()
+
 
 }
 
 
+# x <- dat_raw %>%
+#   filter(!is.na(Recv_Date), Recv_Date != "") %>%
+#   mutate(Recv_Date = dmy(Recv_Date))
+#
+# filter(x, is.na(Recv_Date))
 
 
 
